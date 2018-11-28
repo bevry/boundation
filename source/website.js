5caa03f827cbf0bb446e12dafb4cc33de50adab1
Benjamin Lupton <b@lupton.cc>
2018-11-29 01:43:36 +1100

    specify for now.sh to use version 1

--- source/website.js
+++ source/website.js
@@ -5,8 +5,15 @@ async function updateWebsite(state) {
 
 	// add website deployment strategies
 	if (answers.deploy && answers.deploy.startsWith('now')) {
+		// @todo add support for now v2
+		// https://zeit.co/docs/v2/deployments/official-builders/node-js-now-node/
+		// https://zeit.co/docs/v2/deployments/official-builders/static-now-static/
+		// https://zeit.co/docs/v2/deployments/official-builders/next-js-now-next/
+		// @todo add support for
+		// https://zeit.co/docs/v1/static-deployments/configuration/#redirects-(array)
 		packageData.now = Object.assign(
 			{
+				version: 1,
 				name: answers.nowName,
 				type: 'static',
 				public: true,
