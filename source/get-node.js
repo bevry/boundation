28d18c8152a1f90e22dcef2bcb8825dcd578c4eb
Benjamin Lupton <b@lupton.cc>
2019-11-13 08:01:59 +0800

    more robust lts node version fetching
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- source/get-node.js
+++ source/get-node.js
@@ -2,6 +2,7 @@
 
 // External
 const { fetch } = require('fetch-h2')
+const Errlop = require('errlop')
 
 // Local
 const now = new Date().getTime()
@@ -15,28 +16,29 @@ function isLTS([version, meta]) {
 	return false
 }
 
-async function getMinimumNodeLTSVersion() {
-	const response = await fetch(
+async function getNodeLTSVersions() {
+	const url =
 		'https://raw.githubusercontent.com/nodejs/Release/master/schedule.json'
-	)
-	const json = await response.json()
-	const lts = Object.entries(json)
-		.find(isLTS)[0]
-		.replace('v', '')
-	return lts
+	try {
+		const response = await fetch(url)
+		const json = await response.json()
+		const lts = Object.entries(json)
+		return lts
+	} catch (err) {
+		throw new Errlop(`failed to fetch node.js LTS releases from ${url}`, err)
+	}
+}
+async function getMinimumNodeLTSVersion() {
+	const lts = await getNodeLTSVersions()
+	return lts.find(isLTS)[0].replace('v', '')
 }
 
 async function getMaximumNodeLTSVersion() {
-	const response = await fetch(
-		'https://raw.githubusercontent.com/nodejs/Release/master/schedule.json'
-	)
-	const json = await response.json()
-	const now = new Date().getTime()
-	const lts = Object.entries(json)
+	const lts = await getNodeLTSVersions()
+	return lts
 		.reverse()
 		.find(isLTS)[0]
 		.replace('v', '')
-	return lts
 }
 
 module.exports = { getMinimumNodeLTSVersion, getMaximumNodeLTSVersion }
